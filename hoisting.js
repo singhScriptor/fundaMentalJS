cd658ccfaf3da55caa41e1b30473fd5fc6ae586d
DHIRAJ KR <dhirajkr1117@gmail.com>
2024-12-20 16:58:36 +0530

    hoisting in javascript

--- hoisting.js
+++ hoisting.js
@@ -9,6 +9,7 @@ hoisting is a method where variable and function declaration will be called firs
 //variable
 
 //global variable
+
 console.log(a); //undefined
 var a = 10;
 
@@ -18,4 +19,18 @@ let b = 20;
 
 console.log(c) // this will give reference error
 const c=30;
+
+// function declaration
+
+greeting('Namaste India') //this will display the output cause function declaration will be called first
+function greeting(message){
+    console.log(message)
+}
+
+//function expression
+
+greet("namaste india") //this will work as a varibale and greet will be undefined at this point
+var greet = function(message){
+    console.log(message)
+}
 
