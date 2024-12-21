6701fb76748adbfc30f48d6b6677c07666a93fa1
DHIRAJ KR <dhirajkr1117@gmail.com>
2024-12-21 22:16:42 +0530

    adding let and const features with description

--- ES6FiveFEATURES/let&Const.js
+++ ES6FiveFEATURES/let&Const.js
@@ -1,12 +1,19 @@
+// 'let' is block-scoped and mmutable
+
 let x=10
-x=32
+
+x=32 //this will print 32 after console.log(x)
+
+let x=42 //this will throw a syntax error because x is already defined
 console.log(x)
 
 
-// 'let' is block-scoped and 'const' is block-scoped and immutable
+//'const' is block-scoped and immutable
 
 const c=20
-c=32
+
+c=32   //this will throw type error because we cannot reassign a value to a const variable
+
+const c=42   //this will thrwo sytax error because c is already defined
+
 console.log(c)
-//this will throw an error because we cannot reassign a value to a const variable
-// Error type will be type Error!
