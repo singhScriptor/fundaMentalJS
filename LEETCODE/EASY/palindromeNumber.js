010a0bb693e6d59ec26dee2a485e35e48329fd75
DHIRAJ KR <dhirajkr1117@gmail.com>
2024-12-28 18:10:49 +0530

    Palindorme in numbers"

--- LEETCODE/EASY/palindromeNumber.js
+++ LEETCODE/EASY/palindromeNumber.js
@@ -1,11 +1,24 @@
-var palindrome=(x)=>{
-    let rev=0,temp=x
-    while(x>0){
-        let digit=x%10
-        rev=rev*10+digit
-        x=Math.floor(x/10)
+// var palindrome=(x)=>{
+//     let rev=0,temp=x
+//     while(x>0){
+//         let digit=x%10
+//         rev=rev*10+digit
+//         x=Math.floor(x/10)
+//     }
+//     return temp===rev
+// }
+// let x=121
+// console.log(palindrome(x))
+
+
+var palindrome = (x) => {
+    let temp = x , rev = 0 ;
+    while(x>0) {
+        let digit = x % 10
+        rev = rev * 10 + digit
+        x = Math.floor(x/10)
     }
-    return temp===rev
+    return temp === rev
 }
-let x=121
+let x=311
 console.log(palindrome(x))
