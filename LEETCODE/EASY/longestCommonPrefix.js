2f8777e7a20447b0e4c847fac53f733ecab5167a
DHIRAJ KR <dhirajkr1117@gmail.com>
2024-12-28 18:45:46 +0530

    longest common Prefix

---
+++ LEETCODE/EASY/longestCommonPrefix.js
@@ -0,0 +1,14 @@
+var CommonPrefix=(strs)=>{
+    if(strs.length===0)return ''
+    let prefix=strs[0]
+    for(let i=1;i<strs.length;i++){
+        while(strs[i].indexOf(prefix) !== 0){
+            prefix=prefix.slice(0,prefix.length-1)
+            if(prefix === '') return ''
+        }
+    }
+    return prefix
+}
+let strs1 = ["flower", "flow", "flight"]; 
+console.log(CommonPrefix(strs1));
+ 
