0b818c771e7d4fc2c278d3e07a495bc8dc465d1d
DHIRAJ KR <dhirajkr1117@gmail.com>
2024-12-28 15:22:22 +0530

    lexical scope and closure difference got cleared

--- closure.js
+++ closure.js
@@ -1,30 +1,62 @@
-// function init() {
-//     var name="Arjun"
-//     return function display(){
-//         return name
-//     }
-// }
-// let displayName=init()
-// console.log(displayName())
+function init() {
+    var name="Arjun"
+    return function display(){
+        return name
+    }
+}
+let displayName=init()
+console.log(displayName())
 
 
-// function outerFunction() {
-//     let outervalue="Helloo"
-//     function innerFunction(){
-//         console.log(outervalue)
-//     }
-//     return innerFunction;
-// }
-// let ans=outerFunction()
-// ans()
+function outerFunction() {
+    let outervalue="Helloo"
+    function innerFunction(){
+        console.log(outervalue)
+    }
+    return innerFunction;
+}
+let ans=outerFunction()
+ans()
 
 
-function outerFunction(){
+function outerFunc(){
     let d="Hello World"
-    return function innerFunction(){
+    return function innerFunc(){
         console.log(d)
     }
 }
-let an=outerFunction()
+let an=outerFunc()
 an()
 //outerFunction()
+
+
+//lexicalScope
+function lexical(){
+    let o='Delulu'
+    function lexo(){
+        console.log(o)
+    }
+    lexo()
+}
+lexical()
+
+//fat arrow  for closure
+const DRJ=()=>{
+    let b="Jai Mata Di"
+    return ()=>{
+        console.log(b)
+    }
+
+}
+let s=DRJ()
+s()
+
+const myF=()=>{
+    let jsi="Hello There!"
+    const jrf=()=>{
+        console.log(jsi)
+    }
+    return jrf
+}
+let m=myF()
+m()
