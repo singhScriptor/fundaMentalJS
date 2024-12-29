b11a17ebb1141f5195892809c2381d11e41e398a
DHIRAJ KR <dhirajkr1117@gmail.com>
2024-12-30 01:12:07 +0530

    indexPositon according to target in an array

---
+++ LEETCODE/EASY/searchInsertPosition.js
@@ -0,0 +1,35 @@
+
+//time complexity O(n)
+let searchInsertPosition=(nums,target)=>{
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]==target){
+            return i
+        }
+    }
+    return -1
+}
+let nums=[1,3,5,6],target=5
+console.log(searchInsertPosition(nums,target))
+
+
+//time complexity O(log n)
+
+const indexPosition=(nums,target)=>{
+    let left=0,right=nums.length-1
+    while(left<right){
+        let mid=Math.floor((left+right)/2)
+        if(nums[mid]==target){
+            return mid
+        }
+        else if(nums[mid]<target){
+            left=mid+1
+        }
+        else{
+            right=mid-1
+        }
+    }
+    return left
+}
+let n = [1, 3, 5, 6], t = 5;
+console.log(indexPosition(n,t))
+
