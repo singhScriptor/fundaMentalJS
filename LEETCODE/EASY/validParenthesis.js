b2e6ac7f6c08f093f698b3da52967887c9a309a1
DHIRAJ KR <dhirajkr1117@gmail.com>
2024-12-29 20:45:02 +0530

    validParenthesis using replace method

---
+++ LEETCODE/EASY/validParenthesis.js
@@ -0,0 +1,23 @@
+let validParenthesis=(s)=>{
+    while(s.length){
+        let temp=s
+        s=s.replace("{}","")
+        s=s.replace("[]","")
+        s=s.replace("()","")
+        if(s===temp)return false
+    }
+    return true
+}
+let s='{}{]'
+console.log(validParenthesis(s))
+
+
+//use of replace method demonstration
+//Note : replace method will only use with string not with numbers"
+
+//example :
+let str='Hello World'
+let ans=str.replace('World','Universe')
+console.log(ans)
+
+
