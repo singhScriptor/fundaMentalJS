f3f2db5edfe6e54c7d803c4bc2402598e50e1903
DHIRAJ KR <dhirajkr1117@gmail.com>
2025-01-02 17:44:47 +0530

    function declaration and function expression

---
+++ fnDeclarationVsReference.js
@@ -0,0 +1,28 @@
+//function declaration
+
+function Hello(message){ 
+    console.log(message)
+}
+let message="Hello World"
+Hello(message)
+
+//2nd example
+function James(){
+    console.log('James is bond')
+}
+James()
+
+
+//function reference
+
+let decl=()=>{
+    console.log('Hey There')
+}
+decl()
+
+let decl2=(mes)=>{
+    console.log(mes)
+}
+let mes="Topper is me"
+decl2(mes)
+
