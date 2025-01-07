26e75ceb99894e08f7e0158f28befe1a0d298184
DHIRAJ KR <dhirajkr1117@gmail.com>
2025-01-07 16:21:45 +0530

    setTimeout features

--- setTimeOut.js
+++ setTimeOut.js
@@ -1,25 +1,25 @@
-//function y(){
-//     //in for loop we have used blocked scope let which have created new unique i 
-//     for(let i=1;i<=5;i++){
-//         setTimeout(()=>{
-//             console.log(i)
-//         },i*3000)
-//     }
-//     console.log("Hello World")
-// }
-// y()
+function y(){
+    //in for loop we have used blocked scope let which have created new unique i 
+    for(let i=1;i<=5;i++){
+        setTimeout(()=>{
+            console.log(i)
+        },i*3000)
+    }
+    console.log("Hello World")
+}
+y()
 
-// function z(){
-//     //var is a global scope which wroking as function scope here and i will be same for each iteration.
-//     for(var i=1;i<=5;i++){
-//         setTimeout(()=>{
-//             console.log(i)
-//         },1000)
+function z(){
+    //var is a global scope which wroking as function scope here and i will be same for each iteration.
+    for(var i=1;i<=5;i++){
+        setTimeout(()=>{
+            console.log(i)
+        },1000)
        
-//     }
-//     console.log('Hey There!')
-// }
-// z()
+    }
+    console.log('Hey There!')
+}
+z()
 
 //using closure
 function x(){
