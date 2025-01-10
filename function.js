adb7b8251aa3d8fd7bbf1a25a2a110dfb14b38d9
DHIRAJ KR <dhirajkr1117@gmail.com>
2025-01-10 14:36:49 +0530

    function in more details

--- function.js
+++ function.js
@@ -38,3 +38,67 @@ var v=function(){
     console.log("COme Here")
 }
 v() 
+
+
+//function expression
+
+// named function expression
+let answer=function name(){
+    console.log('Hi Bunny')
+}
+answer()
+
+
+//arrow function expression
+let z=()=>{
+    console.log('Hey There')
+}
+z()
+
+
+//basic function expression
+let p=function(){
+    console.log("Go there")
+}
+p()
+
+/* function Declaration  also known as function statement*/
+function meet(){
+    console.log('Come to meet me')
+}
+meet()
+
+function m(a,c){ //here a,c is paramenter
+
+}
+meet(v,p)//here v,p is arguments
+
+/*
+we can return function from a function and can also able to use function as an arguments.
+*/
+
+//Example
+function outerFunction(){
+    return function(){
+        console.log('Ineer function')
+    }
+}
+//outerFunction()() // direct invocation
+let innerFunction=outerFunction()//storing in variable
+innerFunction()
+
+
+//first class function
+
+/*
+
+the abilty to use as value and assigned it to as variable can passed as an argument and can be returned out from another function is first class function.
+
+*/
+
+
+
+
+
+
+
